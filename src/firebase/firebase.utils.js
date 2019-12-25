fc246b392ad97f1b3f3706e585653a3a31f4b6de
Brayden Ooi <brayden.ooi@hotmail.com>
2019-12-25 18:15:24 +0800

    moved our directory and shop data into its reducer, created respective selectors and updated corresponding components with redux flow for directory/shop

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -25,7 +25,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   const snapShot = await userRef.get();
 
-  if(!snapShot.exists) {
+  if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createAt = new Date();
 
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,16 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
+import { Route } from "react-router-dom";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {
-          collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={ id} { ...otherCollectionProps } />
-          ))
-        }
-      </div>
-    );
-  }
-}
 
 export default ShopPage;
